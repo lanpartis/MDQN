db165f697e34f555dbb49cc1720936aa8a7cd50c
jiao.yongcheng <jiaoyongcheng@gmail.com>
2017-10-19 17:06:07 +0900

    auto forward

--- MDQN_k/model_training_main.py
+++ MDQN_k/model_training_main.py
@@ -9,7 +9,7 @@ from agent import DQNAgent
 #     |_memory
 #         |_action.dat
 #         |_reward.dat
-
+forward = True
 def main():
     epi_file=open('../files/episode.txt')
     episode = epi_file.read(1)
@@ -20,9 +20,15 @@ def main():
     qagent.load_memory_of_episode(episode)
     for i in range(0,len(qagent.memory),qagent.batch_size):
         qagent.memory_replay()
-        if i%5==0:
+        if i%10==0:
             qagent.update_targer_model()
+    qagent.update_targer_model()
     qagent.save_model(episode)
+    if forward:
+        epi_file=open('../files/episode.txt'，'w')
+        epi_file.write(str(episode+1))
+        epi_file.close()
+
 if __name__ == '__main__':
     #select what to train
     #set memory path
