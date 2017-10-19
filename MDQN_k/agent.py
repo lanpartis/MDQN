7b5ba7e8936760d32bb92b53cb8d4c6c62bc3909
jiao.yongcheng <jiaoyongcheng@gmail.com>
2017-10-19 15:46:51 +0900

    fix depth path

--- MDQN_k/agent.py
+++ MDQN_k/agent.py
@@ -87,7 +87,7 @@ class DQNAgent:
         for step in range(0,steps):
             for i in range(1,r_len+1):
                 y_image_path = path+'/RGB/ep'+str(episode)+'/image_' + str(step+1) + '_' + str(i) + '.png'
-                d_image_path = path+'/depth/ep'+str(episode)+'/depth_' + str(step+1) +'_' + str(i) +'.png'
+                d_image_path = path+'/Depth/ep'+str(episode)+'/depth_' + str(step+1) +'_' + str(i) +'.png'
                 y_image = image.imread(y_image_path)
                 d_image = image.imread(d_image_path)
                 y_image = transform.resize(y_image,(198,198),mode='reflect')
