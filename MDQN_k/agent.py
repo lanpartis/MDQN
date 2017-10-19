f5889215d1423527f371c0e96145f44baf68e1cb
jiao.yongcheng <jiaoyongcheng@gmail.com>
2017-10-19 15:56:06 +0900

    debug

--- MDQN_k/agent.py
+++ MDQN_k/agent.py
@@ -86,17 +86,16 @@ class DQNAgent:
         shape.extend(input_shape)
         images = np.zeros(shape)
         depths = np.zeros(shape)
-        for step in range(0,steps):
-            for i in range(1,r_len+1):
-                y_image_path = path+'/RGB/ep'+str(episode)+'/image_' + str(step+1) + '_' + str(i) + '.png'
-                d_image_path = path+'/Depth/ep'+str(episode)+'/depth_' + str(step+1) +'_' + str(i) +'.png'
-                y_image = image.imread(y_image_path)
-                d_image = image.imread(d_image_path)
-                y_image = transform.resize(y_image,(198,198),mode='reflect')
-                d_image = transform.resize(d_image,(198,198),mode='reflect')
-                images[step,:,:,i-1]=y_image
-                depths[step,:,:,i-1]=d_image
-        debug_print('image loading finished.\n')
+        # for step in range(0,steps):
+        for i in range(1,r_len+1):
+            y_image_path = path+'/RGB/ep'+str(episode)+'/image_' + str(step+1) + '_' + str(i) + '.png'
+            d_image_path = path+'/Depth/ep'+str(episode)+'/depth_' + str(step+1) +'_' + str(i) +'.png'
+            y_image = image.imread(y_image_path)
+            d_image = image.imread(d_image_path)
+            y_image = transform.resize(y_image,(198,198),mode='reflect')
+            d_image = transform.resize(d_image,(198,198),mode='reflect')
+            images[step,:,:,i-1]=y_image
+            depths[step,:,:,i-1]=d_image
         return images,depths
 
     def memorize(self,state,action,reward,n_state):
@@ -196,7 +195,7 @@ class DQNAgent:
                 next_state=np.concatenate((y_,d_),axis=0)
                 terminal=False
             self.memorize(state,action[episode-1][step],reward[episode-1][step],next_state,terminal)
-            debug_print('memory of episode %d step %d loaded'%{episode,step+1})
+            debug_print('memory of episode %d step %d loaded'%(episode,step+1))
 
 
 def read_dat_file(path):
