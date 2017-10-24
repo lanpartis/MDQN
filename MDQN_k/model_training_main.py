0898d6e263716d36c097b4694412ae2b7e04ed53
jiao.yongcheng <jiaoyongcheng@gmail.com>
2017-10-24 13:00:10 +0900

    m

--- MDQN_k/model_training_main.py
+++ MDQN_k/model_training_main.py
@@ -18,16 +18,11 @@ def main():
     qagent = DQNAgent(episode-1)
     qagent.load_memory_of_episode(episode)
     for k in range(5):
-        for j in range(10):
+        for j in range(5):
             for i in range(0,len(qagent.memory),qagent.batch_size):
                 qagent.memory_replay()
         qagent.update_targer_model()
-    # for i in range(0,len(qagent.memory),qagent.batch_size):
-    #     qagent.memory_replay()
-    #     if i%10==0:
-    #         qagent.update_targer_model()
     qagent.save_model(episode)
-    print("Training finished")
     if forward:
         epi_file=open('../files/episode.txt','w')
         epi_file.write(str(episode+2))
