c0c8d212e59d5c6a5af498e17511c2e2cb132470
lan <lan@lans-MacBook-Pro.local>
2017-12-04 17:36:59 +0900

    m

--- MDQN_k/model_training_main.py
+++ MDQN_k/model_training_main.py
@@ -34,7 +34,7 @@ def main():
         total4+=t4
         correct4+=c4
     qagent.save_model(episode)
-    res = time.strftime('%Y/%m/%d-%H:%M:%S',time.localtime(time.time()))+"Average of episode: %d Q_y: %f Q_d: %f"%(episode,np.mean(qys),np.mean(qds))
+    res = time.strftime('%Y/%m/%d-%H:%M:%S',time.localtime(time.time()))+"Average of episode: %d Q_y: %f Q_d: %f,correct shake: %f"%(episode,np.mean(qys),np.mean(qds),correct4/total4)
     epi_file=open('../files/avg_Q.txt','a')
     epi_file.write(res+'\n')
     epi_file.close()
